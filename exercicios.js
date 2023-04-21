efcdc708ad12205300a1e10227428b8af6afe9c7
rafael <rafael.eustapinto@gmail.com>
2023-04-21 19:44:06 -0300

    Parte III - Objetos & Arrays

--- exercicios.js
+++ exercicios.js
@@ -61,41 +61,96 @@ function max(a, b) {
     else return b;
 }
 
-function mod2(number){
+function mod2(number) {
     number = Math.abs(number) - 2
     number = Math.abs(number)
 
-    if (number === 0){
+    if (number === 0) {
         return true;
-    }else if (number < 2){
+    } else if (number < 2) {
         return false;
-    }else{
+    } else {
         return mod2(number)
     }
 }
 
-function mod(num, modl){
-    if(modl === 0) return NaN
+function mod(num, modl) {
+    if (modl === 0) return NaN
 
     num = Math.abs(num) - modl
     num = Math.abs(num)
 
-    if (num === 0){
+    if (num === 0) {
         return true;
-    }else if (num < modl){
+    } else if (num < modl) {
         return false;
-    }else{
+    } else {
         return mod(num, modl)
     }
 }
 
-function countChars(frase, c){
+function countChars(frase, c) {
     let count = 0;
-    for (let i = 0; i < frase.length; i++){
-        if(frase[i] === c){
+    for (let i = 0; i < frase.length; i++) {
+        if (frase[i] === c) {
             count++;
         }
     }
     return count;
 }
+
+
+// Objetos & Arrays
+
+function range(min, max, i) {
+    numbers = []
+    for (let j = min + 1; j < max; j += i) {
+        numbers.push(j)
+    }
+    return numbers
+}
+
+function reverseArray(array) {
+    return array.reverse()
+}
+
+function toList(array) {
+    let list = {}
+    for (let i = 0; i < array.length; i++) {
+        let element = list
+        // Percorre os nodos
+        for (let j = 0; j < i; j++) {
+            if (element.rest === undefined || element.rest === null) element.rest = {}
+            element = element.rest
+        }
+
+        element.value = array[i]
+        element.rest = null
+    }
+
+    return list
+}
+
+function deepEquals(obj1, obj2) {
+    // Se são o mesmo objeto retorna true
+    if (obj1 === obj2) return true;
+
+    // Checa se são primivos, se sim compara
+    if ((obj1 !== Object(obj1)) && (obj2 !== Object(obj2))) return obj1 === obj2;
+
+    // Checa se o número de atributos eh o mesmo, se não retorna falso
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+
+    // Compara os objetos
+    for (let key in obj1) {
+        //Checa se o outro objeto tem essa propriedade
+        if(!(key in obj2)) return false; 
+
+        //Retorna falso se as propriedades forem diferentes, usando recursividade
+        if(!deepEquals(obj1[key], obj2[key])) return false;
+    }
+
+    return true;
+}
+
 
