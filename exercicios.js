a1249d1c0d07afb64ca522141dc37ae94fa1512c
rafael <rafael.eustapinto@gmail.com>
2023-04-21 18:47:33 -0300

    Parte II - Funções

--- exercicios.js
+++ exercicios.js
@@ -1,23 +1,26 @@
+
+// Conceitos básicos
+
 function exercicio_triangulo(lines) {
     for (let i = 1; i <= lines; i++) {
-        let msg = ''
+        let msg = '';
         for (let j = 0; j <= i; j++) {
-            msg += '#'
+            msg += '#';
         }
-        console.log(msg)
+        console.log(msg);
     }
 }
 
 function exercicio_xadrez(lines) {
-    var tline1, tline2 = ''
+    var tline1, tline2 = '';
     for (let i = 0; i < lines; i++) {
-        let msg = ''
+        let msg = '';
         for (let j = 0; j < lines; j++) {
             if ((j + i) % 2 == 0) {
-                msg += '#'
+                msg += '#';
             }
             else {
-                msg += ' '
+                msg += ' ';
             }
         }
         console.log(msg)
@@ -25,26 +28,74 @@ function exercicio_xadrez(lines) {
 }
 
 function exercicio_verifica_palindromo(text) {
-    // console.log(text)
-    // console.log(text.split('').reverse().join(''))
 
-    text = text.split('').filter((letra) => letra !== " ")
+    text = text.split('').filter((letra) => letra !== " ");
 
-    return text.join('') == text.reverse().join('')
+    return text.join('') == text.reverse().join('');
 }
 
 function exercicio_programa_diferente() {
     for (let i = 0; i < 100; i++) {
-        if (i % (3 * 5) == 0){
-            console.log('FizzBuzz')
-        }else if( i % 3 == 0){
-            console.log('Fizz')
-        }else if( i % 5 == 0){
-            console.log('Buzz')
-        }else {
-            console.log(i)
+        if (i % (3 * 5) == 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 == 0) {
+            console.log('Fizz');
+        } else if (i % 5 == 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
         }
 
     }
+}
+
+// Funções
+
+function min(a, b) {
+    if (a < b) return a;
+    else return b;
+}
+
+function max(a, b) {
+    if (a > b) return a;
+    else return b;
+}
+
+function mod2(number){
+    number = Math.abs(number) - 2
+    number = Math.abs(number)
+
+    if (number === 0){
+        return true;
+    }else if (number < 2){
+        return false;
+    }else{
+        return mod2(number)
+    }
+}
+
+function mod(num, modl){
+    if(modl === 0) return NaN
+
+    num = Math.abs(num) - modl
+    num = Math.abs(num)
+
+    if (num === 0){
+        return true;
+    }else if (num < modl){
+        return false;
+    }else{
+        return mod(num, modl)
+    }
+}
+
+function countChars(frase, c){
+    let count = 0;
+    for (let i = 0; i < frase.length; i++){
+        if(frase[i] === c){
+            count++;
+        }
+    }
+    return count;
 }
 
